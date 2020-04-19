2bcf392fd951c79f1d62a75fd453b190d7c140a6
Jeffrey Stevison <jeffrey@stevison.com>
2020-04-19 17:25:59 -0500

    RandomColors UINavigationController
    RandomColors UINavigationController

---
+++ RandomColors/RandomColors/ColorsDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  ColorsDetailVC.swift
+//  RandomColors
+//
+//  Created by Jeffrey Stevison on 4/19/20.
+//  Copyright © 2020 Jeffrey Stevison. All rights reserved.
+//
+
+import UIKit
+
+class ColorsDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
