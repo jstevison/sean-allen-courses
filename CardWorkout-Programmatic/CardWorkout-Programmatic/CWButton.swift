bf8718c09fc9cd318070d6cdb19d3639796f94aa
Jeffrey Stevison <jeffrey@stevison.com>
2020-04-20 13:32:22 -0500

    CardWorkout-Programmatic Programmatic - Custom UIButton
    Programmatic - Custom UIButton

---
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,34 @@
+//
+//  CWButton.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Jeffrey Stevison on 4/20/20.
+//  Copyright © 2020 Jeffrey Stevison. All rights reserved.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false // use autolayout
+    }
+}
